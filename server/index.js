97598a077f75d99ff7940009e5fbcfdbbcf7da1f
Mark Oliver <mark.c.oliver@gmail.com>
2018-03-25 18:31:20 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -1,41 +1,42 @@
 const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+const apiRouter = require('./common/apiRouter.js');
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.json());
+
+server.use('', apiRouter);
+server.use(express.static(__dirname + '/my-budget/build/'));
+const MongoClient = require('mongodb').MongoClient;
+const AtlasMongoIUrl = 
+'mongodb://LambdaDataManager:XPoCMUH4qMHINDKc@mongoi-shard-00-00-2ydlw.mongodb.net:27017,mongoi-shard-00-01-2ydlw.mongodb.net:27017,mongoi-shard-00-02-2ydlw.mongodb.net:27017/mongoi?ssl=true&replicaSet=mongoi-shard-0&authSource=admin';
+   //          'mongodb://BBData:1715m6kPqfppaHy4@bb-lambda0-shard-00-00-gdvzr.mongodb.net:27017,bb-lambda0-shard-00-01-gdvzr.mongodb.net:27017,bb-lambda0-shard-00-02-gdvzr.mongodb.net:27017/BBTest?ssl=true&replicaSet=BB-Lambda0-shard-0&authSource=admin';
+//const accountCollection = 'BBCollection'
+const accountCollection = 'accounts'
+/*
+MongoClient.connect(AtlasMongoIUrl, (err,db) => {
+  if (err) {
+    console.log('connect err:', err)
+    throw err
   }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  */
+//const collection = db.collection()
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(AtlasMongoIUrl + `&collection=${accountCollection}`, { useMongoClient: true })
+  .then((db) => {
+    console.log('All your databases are belong to us!');
+    const port = 5003
+    server.listen(process.env.port || port, function() {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(function(err) {
+    console.log('Database connection failed', err.message);
   });
-
-} else {
-  const app = express();
-
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
-
-  app.listen(PORT, function () {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-  });
-}
