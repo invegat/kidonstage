01518ff4c7e69d44f1d9ba956a88bb095779ff64
Mark Oliver <mark.c.oliver@gmail.com>
2018-03-25 18:55:42 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
@@ -12,11 +14,10 @@ server.use(cors());
 server.use(bodyParser.json());
 
 server.use('', apiRouter);
-server.use(express.static(__dirname + '/my-budget/build/'));
 const MongoClient = require('mongodb').MongoClient;
-const AtlasMongoIUrl = 
-'mongodb://LambdaDataManager:XPoCMUH4qMHINDKc@mongoi-shard-00-00-2ydlw.mongodb.net:27017,mongoi-shard-00-01-2ydlw.mongodb.net:27017,mongoi-shard-00-02-2ydlw.mongodb.net:27017/mongoi?ssl=true&replicaSet=mongoi-shard-0&authSource=admin';
-   //          'mongodb://BBData:1715m6kPqfppaHy4@bb-lambda0-shard-00-00-gdvzr.mongodb.net:27017,bb-lambda0-shard-00-01-gdvzr.mongodb.net:27017,bb-lambda0-shard-00-02-gdvzr.mongodb.net:27017/BBTest?ssl=true&replicaSet=BB-Lambda0-shard-0&authSource=admin';
+const AtlasMongoIUrl =
+  'mongodb://LambdaDataManager:XPoCMUH4qMHINDKc@mongoi-shard-00-00-2ydlw.mongodb.net:27017,mongoi-shard-00-01-2ydlw.mongodb.net:27017,mongoi-shard-00-02-2ydlw.mongodb.net:27017/mongoi?ssl=true&replicaSet=mongoi-shard-0&authSource=admin';
+//          'mongodb://BBData:1715m6kPqfppaHy4@bb-lambda0-shard-00-00-gdvzr.mongodb.net:27017,bb-lambda0-shard-00-01-gdvzr.mongodb.net:27017,bb-lambda0-shard-00-02-gdvzr.mongodb.net:27017/BBTest?ssl=true&replicaSet=BB-Lambda0-shard-0&authSource=admin';
 //const accountCollection = 'BBCollection'
 const accountCollection = 'accounts'
 /*
@@ -31,12 +32,15 @@ mongoose.Promise = global.Promise;
 mongoose
   .connect(AtlasMongoIUrl + `&collection=${accountCollection}`, { useMongoClient: true })
   .then((db) => {
+    // Priority serve any static files.
+    server.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
     console.log('All your databases are belong to us!');
     const port = 5003
-    server.listen(process.env.port || port, function() {
+    server.listen(process.env.port || port, function () {
       console.log(`Server running on port ${port}`);
     });
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log('Database connection failed', err.message);
   });
